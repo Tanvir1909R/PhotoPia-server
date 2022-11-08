19903721ae18eb45668036fe56d07ba4b370ab66
Tanvir1909R <trcr719@gmail.com>
2022-11-08 15:43:20 +0600

    add to mongo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +10,26 @@ app.use(cors())
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.5ki2fpf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 app.get('/',(req,res)=>{
     res.send('welcome to server')
 })
+
+const db = async ()=>{
+    const servicesCollection = client.db('photoPia').collection('services');
+
+    app.get('/services', async(req, res)=>{
+        const filter = {};
+        const result = servicesCollection.find(filter);
+        const services = await result.toArray();
+        res.send(services)
+    })
+}
+
+db().catch(console.dir);
 
 app.listen(port, ()=>{
     console.log('server is ready');
